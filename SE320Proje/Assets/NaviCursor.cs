8f8a73d9173d44adf5016f44293f9c0d370cbf43
furkanmakasci <73423746+furkanmakasci@users.noreply.github.com>
2021-01-13 16:26:26 +0300

    control change and tower menu
    control change and tower menu

---
+++ SE320Proje/Assets/NaviCursor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaviCursor : MonoBehaviour
+{
+    public GameObject towerMenuDealer;
+    public GameObject towerAddingPanel;
+    public GameObject player;
+
+    bool eMenu=false;
+    bool pMenu=false;
+
+    public Transform cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void button1Click()
+    {
+        towerAddingPanel.SetActive(false);
+        pMenu = false;
+        Time.timeScale = 1f;
+        player.GetComponent<Control>().enabled = true;
+        Cursor.visible = false;
+    }
+
+    public void button2Click()
+    {
+        towerAddingPanel.SetActive(false);
+        pMenu = false;
+        Time.timeScale = 1f;
+        player.GetComponent<Control>().enabled = true;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(cursor.transform.position, cursor.transform.forward, out hit, 5);
+        var whatBeenHit = hit.transform.gameObject.GetComponent<EmptyForCursor>();
+
+
+
+
+        if (whatBeenHit != null && pMenu == false)
+        {
+            towerMenuDealer.SetActive(true);
+            eMenu = true;
+            if (Input.GetKeyDown(KeyCode.E) && eMenu == true)
+            {
+                player.GetComponent<Control>().enabled = false;
+                Cursor.visible = true;
+                towerMenuDealer.SetActive(false);
+                eMenu = false;
+                towerAddingPanel.SetActive(true);
+                pMenu = true;
+                Time.timeScale = 0f;
+
+            }
+            
+        }
+        if (whatBeenHit == null)
+        {
+            towerMenuDealer.SetActive(false);
+            eMenu = false;
+        }
+
+    }
+}
--- SE320Proje/Assets/Scripts/Control.cs
+++ SE320Proje/Assets/Scripts/Control.cs
@@ -5,26 +5,21 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform thing;
+    public Transform ccamera;
 
     public float speed = 6f;
+
     public float turnSmoothTime = .1f;
     float turnSmoothVelocity;
     
-    public Transform feet;
-    public float distance2ground = 0.3f;
-    public LayerMask groundMask;
     public float gravity = -9.81f;
 
-    private Vector3 velocity;
-
-    private bool isGrounded;
+    private Vector3 gravityV;
+    private Vector3 jumpV;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        
-        
     }
 
     // Update is called once per frame
@@ -35,21 +30,35 @@ public class Control : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= .1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + thing.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + ccamera.eulerAngles.y;
+            //getting angle
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
            
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            //facing forward
+            controller.Move((moveDirection.normalized+gravityV+jumpV) * speed * Time.deltaTime);
 
         }
         
-        isGrounded = Physics.CheckSphere(feet.position, distance2ground, groundMask);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-        if (isGrounded && velocity.y < 0)
+        gravityV.y += gravity * Time.deltaTime;
+
+        if (controller.isGrounded)
         {
-            velocity.y = -2f;
+            if (Input.GetKeyDown("space"))
+            {
+                jumpV = Vector3.up * 4;
+            }
+            else
+            {
+                jumpV = Vector3.zero;
+            }
+
+            gravityV.y = -2f;
         }
+
+        controller.Move((gravityV + jumpV) * speed * Time.deltaTime);
+
+
     }
 }
